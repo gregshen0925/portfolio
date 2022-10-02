1db9555cccf98b485dac3ed851dae414fbfe4cea
GregShen <gregshen0925@gmail.com>
2022-10-02 19:10:52 +0800

    background circle

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Cursor, useTypewriter } from "react-simple-typewriter"
+import BackgroundCircles from './BackgroundCircles'
+import Image from "next/image"
+import seaTurtle from "../src/seaTurtle.jpg"
+type Props = {}
+
+export default function Hero({ }: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, This Is Greg Shen",
+      "Guy-who-loves-everything",
+      "<Keep_Learning_Keep_Coding />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  })
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center 
+    text-center overflow-hidden">
+      <BackgroundCircles />
+      <div className='relative h-32 w-32 mx-auto'>
+        <Image
+          src={seaTurtle}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+          alt=""
+        />
+      </div>
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Founder Of InJoy Labs
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+      </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    overflow-scroll z-0">
       <Head>
-        <title>Portfolio of Greg</title>
+        <title>Greg's Portfolio</title>
       </Head>
 
-      {/* Header */}
       <Header />
 
       {/* Hero */}
+      <section id='hero' className="snap-center">
+        <Hero />
+      </section>
 
       {/* About */}
 
@@ -24,8 +28,6 @@ const Home: NextPage = () => {
 
       {/* Contact Me */}
 
-
-      <h1 className="text-blue-500">Building AWESOME profile</h1>
     </div>
   )
 }
