a5942a5c342eca866ab78bf2c2262e1e40d09aea
GregShen <gregshen0925@gmail.com>
2022-10-02 21:17:55 +0800

    about page

--- components/About.tsx
+++ components/About.tsx
@@ -1,10 +1,48 @@
+import { MotionConfig } from 'framer-motion'
 import React from 'react'
+import { motion } from "framer-motion"
+
 
 type Props = {}
 
 const About = (props: Props) => {
     return (
-        <div>About</div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            className='flex flex-col relative h-screen text-center md:text-left
+            md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+                About
+            </h3>
+
+            <motion.img
+                initial={{
+                    x: -200,
+                    opacity: 0,
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80"
+                className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+                md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+            />
+            <div className='space-y-10 px-0 md:px-10'>
+                <h4 className='text-4xl font-semibold'>
+                    Here is a{" "}
+                    <span className='underline decoration-[#F7aB0A]/50'>little</span>{" "}
+                    background
+                </h4>
+                <p className='text-sm'>
+                    Hi, I'm Greg. Yo
+                </p>
+            </div>
+
+        </motion.div>
     )
 }
 
