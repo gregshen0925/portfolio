41745c4e3041ab38a7c2ef6769be40cf15221f06
GregShen <gregshen0925@gmail.com>
2022-10-02 23:52:26 +0800

    experiences

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Experiences from '../components/Experiences'
+import Skills from '../components/Skills'
 
 const Home: NextPage = () => {
   return (
@@ -15,21 +16,22 @@ const Home: NextPage = () => {
 
       <Header />
 
-      {/* Hero */}
       <section id='hero' className="snap-start">
         <Hero />
       </section>
 
-      {/* About */}
       <section id="about" className='snap-center'>
         <About />
       </section>
 
-      {/* Experience */}
       <section id="experience" className='snap-center'>
         <Experiences />
       </section>
-      {/* Skills */}
+
+      <section id="skills" className='snap-start'>
+        <Skills />
+      </section>
+
 
       {/* Projects */}
 
