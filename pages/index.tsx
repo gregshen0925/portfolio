1624687d4a8ac1f58d05a98896114d38dcf84e70
GregShen <gregshen0925@gmail.com>
2022-10-03 19:05:50 +0800

    add utils and getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import About from '../components/About'
 import Header from '../components/Header'
@@ -10,8 +10,23 @@ import ContactMe from '../components/ContactMe'
 import Link from 'next/link'
 import seaTurtle from '../src/seaTurtle.jpg'
 import Image from 'next/image'
+import { Experience, PageInfo, Project, Skill, Social } from '../typings'
+import { fetchPageInfo } from '../utils/fetchPageInfo'
+import { fetchExperiences } from '../utils/fetchExperiences';
+import { fetchProjects } from '../utils/fetchProjects';
+import { fetchSocials } from '../utils/fetchSocials'
+import { fetchSkills } from '../utils/fetchSkills';
 
-const Home: NextPage = () => {
+
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+}
+
+const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
     overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
@@ -65,3 +80,22 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+  }
+}
