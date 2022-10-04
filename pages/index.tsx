4880e09f33181338b378117fd05ce4a3c8c15360
GregShen <gregshen0925@gmail.com>
2022-10-04 15:55:24 +0800

    bg to black

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ type Props = {
 
 const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    <div className="bg-[rgb(2,2,2)] text-white h-screen snap-y snap-mandatory
     overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
      scrollbar-thumb-[#F7AB0A]">
       <Head>
