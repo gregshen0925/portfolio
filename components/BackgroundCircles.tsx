ce80dfba0289fb152428ea7b45b736cba9d031ca
GregShen <gregshen0925@gmail.com>
2022-10-06 12:42:05 +0800

    backgroundCircle

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -27,18 +27,8 @@ const BackgroundCircles = (props: Props) => {
         mt-52 animate-pulse"/>
             <div className="rounded-full border border-[#333333] opacity-20 h-[360px] w-[360px] md:h-[650px] md:w-[650px]
         absolute mt-52"/>
-            <div className="rounded-full border border-[#333333] h-[400px] w-[400px] md:h-[800px] md:w-[800px] absolute 
+            <div className="rounded-full border border-[#333333] h-[400px] w-[400px] md:h-[700px] md:w-[700px] absolute 
         mt-52 "/>
-            {/* <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] 
-        mt-52 animate-ping"/>
-            <div className="rounded-full border border-[#333333] h-[300px] w-[300px] absolute 
-        mt-52"/>
-            <div className="rounded-full border border-[#333333] h-[500px] w-[500px] absolute 
-        mt-52"/>
-            <div className="rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] 
-        absolute mt-52 animate-pulse"/>
-            <div className="rounded-full border border-[#333333] h-[800px] w-[800px] absolute 
-        mt-52"/> */}
         </motion.div>
     )
 }
